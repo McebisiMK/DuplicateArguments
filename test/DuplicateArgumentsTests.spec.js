410b8a560710c96a1856264c15bf864dc537d460
Mcebisi Mkhohliwe <McebisiM@SAHOMELOANS.COM>
2018-10-08 15:16:05 +0200

    Test Refactoring

--- test/DuplicateArgumentsTests.spec.js
+++ test/DuplicateArgumentsTests.spec.js
@@ -1,6 +1,6 @@
 describe("Duplicates", function () {
     describe("Solution", function () {
-        describe("Empty array", function () {
+        describe("Given empty array", function () {
             it("Should return false", function () {
                 //----------------Arrange------------
                 let numbers = [];
@@ -15,14 +15,13 @@ describe("Duplicates", function () {
             })
         })
 
-        describe("Given Array with single number", function () {
+        describe("Given an array containing a single number", function () {
             [{ numbers: [1], output: false },
             { numbers: [10], output: false },
             { numbers: [100], output: false }]
                 .forEach(function (element) {
                     it("Should return false", function () {
                         //----------------Arrange------------
-                        let numbers = [];
                         let duplicates = new Duplicates();
 
                         //----------------Act----------------
@@ -35,14 +34,13 @@ describe("Duplicates", function () {
                 })
         })
 
-        describe("Given Array with containing unique numbers", function () {
+        describe("Given an array containing unique numbers", function () {
             [{ numbers: [1, 5], output: false },
             { numbers: [10, 69, 45], output: false },
             { numbers: [100, 89, 32, 69], output: false }]
                 .forEach(function (element) {
                     it("Should return false", function () {
                         //----------------Arrange------------
-                        let numbers = [];
                         let duplicates = new Duplicates();
 
                         //----------------Act----------------
@@ -55,14 +53,13 @@ describe("Duplicates", function () {
                 })
         })
 
-        describe("Given Array containing identical numbers", function () {
+        describe("Given an array containing identical numbers", function () {
             [{ numbers: [1, 2, 3, 2], output: true },
             { numbers: [1, 5, 1, 6, 1, 3], output: true },
             { numbers: [4, 5, 4, 6, 4, 7, 4, 8], output: true }]
                 .forEach(function (element) {
                     it("Should return false", function () {
                         //----------------Arrange------------
-                        let numbers = [];
                         let duplicates = new Duplicates();
 
                         //----------------Act----------------
@@ -75,14 +72,13 @@ describe("Duplicates", function () {
                 })
         })
 
-        describe("Given Array containing identical numbers as strings", function () {
+        describe("Given an array containing identical numbers as strings", function () {
             [{ numbers: ['1', '2', '3', '2'], output: true },
             { numbers: ['1', '5', '1', '6', '1', '3'], output: true },
             { numbers: ['4', '5', '4', '6', '4', '7', '4', '8'], output: true }]
                 .forEach(function (element) {
                     it("Should return false", function () {
                         //----------------Arrange------------
-                        let numbers = [];
                         let duplicates = new Duplicates();
 
                         //----------------Act----------------
